b503bdb757e1eabbddd49ceeda76262e79a4e17b
Antonio <gantoniodeveloper@outlook.com>
2020-10-11 17:27:52 -0500

    =

--- app/Middleware/DetectSession.js
+++ app/Middleware/DetectSession.js
@@ -17,9 +17,11 @@ class DetectSession {
       const token = await Token.findBy('token', tokenheader)
       
       if(token !== null){
+        
         const user = await User.findBy('id', token.user_id)
-        banverify = await Banlist.findBy('user_id', token.user_id)
-        banverify === null ? request.user={status : '200', message:user} : request.user = {status: '413', message:`You're bannerd for ${banverify.reason}`}
+        const banverify = await Banlist.findBy('user_id', token.user_id)
+
+        banverify == null ? request.user={status : '200', message:user} : request.user = {status: '413', message:`You're bannerd for ${banverify.reason}`}
       }else {
         request.user = {status: '413', message:'Unautorized'}
       }
